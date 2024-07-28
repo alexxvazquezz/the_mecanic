60507e97949c4fe0edf5749c956d4ffddf1e0254
Alejandro Vazquez <alexx.vazquezz@gmail.com>
2024-07-28 00:39:15 -0300

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('config.Config')
 
     db.init_app(app)
